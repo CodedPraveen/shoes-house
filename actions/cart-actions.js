8e53ef3d8bd1766b00ca16d59d110f1cf9e6894f
Project <praveenbishnoi8809@gmail.com>
2026-06-12 17:11:28 +0530

    make new prop for requiredbuser

--- actions/cart-actions.js
+++ actions/cart-actions.js
@@ -1,43 +1,9 @@
 "use server";
 
-import { auth, clerkClient } from "@clerk/nextjs/server";
 import { cartService } from "@/services/cart-service";
-import { userService } from "@/services/user-service";
 import { assertRateLimit } from "@/lib/rate-limit";
 import { withPerf } from "@/lib/perf";
-
-// async function requireDbUser() {
-//   const { userId: clerkId } = await auth();
-//   if (!clerkId) throw new Error("Unauthorized");
-//   const user = await userService.getByClerkId(clerkId);
-//   if (!user) throw new Error("User not synced. Sign in again.");
-//   return user;
-// }
-
-async function requireDbUser() {
-  const { userId: clerkId } = await auth();
-
-  if (!clerkId) {
-    throw new Error("Unauthorized");
-  }
-
-  let user = await userService.getByClerkId(clerkId);
-
-  if (user) return user;
-
-  console.log("AUTO CREATING USER");
-
-  const client = await clerkClient();
-  const clerkUser = await client.users.getUser(clerkId);
-
-  user = await userService.upsertFromClerk({
-    clerkId: clerkUser.id,
-    email: clerkUser.emailAddresses[0].emailAddress,
-    name: `${clerkUser.firstName ?? ""} ${clerkUser.lastName ?? ""}`.trim(),
-  });
-
-  return user;
-}
+import { requireDbUser } from "@/lib/require-db-user";
 
 export async function getCartAction() {
   const user = await requireDbUser();
