5e002053d798e913454433156be16aee9c06ef51
Project <bishnoipraveen8809@gmail.com>
2026-07-28 10:12:19 +0530

    navbar loading successfull

--- components/shop-shell.js
+++ components/shop-shell.js
@@ -1,20 +1,35 @@
+// import NavbarServer from "@/components/navbar-server";
+// import SearchModal from "@/components/search-modal";
+// import SiteFooter from "@/components/site-footer";
+// import { usePathname } from "next/navigation";
+
+// const pathname = usePathname();
+
+// const collection = pathname.startsWith("/jewellery")
+//   ? "JEWELLERY"
+//   : "SHOES";
+// export default function ShopShell({
+//   children,
+//   collection,
+// }) {
+//   return (
+//     <>
+//       <NavbarServer collection={collection} />
+//       <SearchModal />
+//       {children}
+//       <SiteFooter />
+//     </>
+//   );
+// }
+
 import NavbarServer from "@/components/navbar-server";
 import SearchModal from "@/components/search-modal";
 import SiteFooter from "@/components/site-footer";
-import { usePathname } from "next/navigation";
 
-const pathname = usePathname();
-
-const collection = pathname.startsWith("/jewellery")
-  ? "JEWELLERY"
-  : "SHOES";
-export default function ShopShell({
-  children,
-  collection,
-}) {
+export default function ShopShell({ children }) {
   return (
     <>
-      <NavbarServer collection={collection} />
+      <NavbarServer />
       <SearchModal />
       {children}
       <SiteFooter />
