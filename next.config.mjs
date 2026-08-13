60aab0b5b5359ebc08338963af4bbc79f9a46f35
Project <bishnoipraveen8809@gmail.com>
2026-08-13 19:33:07 +0530

    image upload limit up to 10mb

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverActions: {
-    bodySizeLimit: "10mb",
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "10mb",
+    },
   },
+
   images: {
     remotePatterns: [
       {
